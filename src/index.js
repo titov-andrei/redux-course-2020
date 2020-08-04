33602c1245e0e48335b71148689ab71c4f19c0f3
titov-andrei <titov_andrei@mail.ru>
2020-08-04 14:18:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import './styles.css'
 
 const counter = document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('btn')
 
 let state = 0
 
 function render() {
   counter.textContent = state.toString()
 }
+
+addBtn.addEventListener('click', () => { 
+  state++
+  render()
+})
+
+subBtn.addEventListener('click', () => { 
+  state--
+  render()
+})
 
 render()
